925194340a21036e1a10c7b06fb6845e00c5602b
priyangapriyahh-prog <priyangapriyahh@gmail.com>
2026-08-02 21:47:06 +0530

    Add files via upload

---
+++ LAB_PROGRAMS/LAB-4.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+int main()
+{
+    char s[5];
+    printf("Enter any operator: ");
+    scanf("%s", s);
+    switch(s[0])
+    {
+        case '>':
+            if(s[1] == '=')
+                printf("Greater than or equal");
+            else
+                printf("Greater than");
+            break;
+        case '<':
+            if(s[1] == '=')
+                printf("Less than or equal");
+            else
+                printf("Less than");
+            break;
+        case '=':
+            if(s[1] == '=')
+                printf("Equal to");
+            else
+                printf("Assignment");
+            break;
+        case '!':
+            if(s[1] == '=')
+                printf("Not Equal");
+            else
+                printf("Bit Not");
+            break;
+        case '&':
+            if(s[1] == '&')
+                printf("Logical AND");
+            else
+                printf("Bitwise AND");
+            break;
+        case '|':
+            if(s[1] == '|')
+                printf("Logical OR");
+            else
+                printf("Bitwise OR");
+            break;
+        case '+':
+            printf("Addition");
+            break;
+        case '-':
+            printf("Subtraction");
+            break;
+        case '*':
+            printf("Multiplication");
+            break;
+        case '/':
+            printf("Division");
+            break;
+        case '%':
+            printf("Modulus");
+            break;
+        default:
+            printf("Not an operator");
+    }
+    return 0;
+}
