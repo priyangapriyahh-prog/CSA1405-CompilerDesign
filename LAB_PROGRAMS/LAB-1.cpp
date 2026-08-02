972da8d0368373715c7316d2e7f660d78fe7d02b
priyangapriyahh-prog <priyangapriyahh@gmail.com>
2026-08-02 21:45:28 +0530

    Add files via upload

---
+++ LAB_PROGRAMS/LAB-1.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+    int i, ic = 0, m, cc = 0, oc = 0, j;
+    char b[30], operators[30], identifiers[30], constants[30];
+    printf("Enter the string: ");
+    scanf("%[^\n]", b);
+    for (i = 0; i < strlen(b); i++)
+    {
+        if (isspace(b[i]))
+        {
+            continue;
+        }
+        else if (isalpha(b[i]))
+        {
+            identifiers[ic] = b[i];
+            ic++;
+        }
+        else if (isdigit(b[i]))
+        {
+            m = b[i] - '0';
+            i++;
+
+            while (isdigit(b[i]))
+            {
+                m = m * 10 + (b[i] - '0');
+                i++;
+            }
+            i--;
+            constants[cc] = m;
+            cc++;
+        }
+        else
+        {
+            if (b[i] == '*')
+            {
+                operators[oc] = '*';
+                oc++;
+            }
+            else if (b[i] == '-')
+            {
+                operators[oc] = '-';
+                oc++;
+            }
+            else if (b[i] == '+')
+            {
+                operators[oc] = '+';
+                oc++;
+            }
+            else if (b[i] == '=')
+            {
+                operators[oc] = '=';
+                oc++;
+            }
+        }
+    }
+    printf("\nIdentifiers : ");
+    for (j = 0; j < ic; j++)
+    {
+        printf("%c ", identifiers[j]);
+    }
+    printf("\nConstants : ");
+    for (j = 0; j < cc; j++)
+    {
+        printf("%d ", constants[j]);
+    }
+    printf("\nOperators : ");
+    for (j = 0; j < oc; j++)
+    {
+        printf("%c ", operators[j]);
+    }
+    return 0;
+}
